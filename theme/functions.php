908899e4db74ced5c6c4fe1a5060a77f9491d419
artovenry <artovenry@gmail.com>
2015-09-23 20:20:40 +0900

    commit

--- theme/functions.php
+++ theme/functions.php
@@ -4,4 +4,19 @@ require "php/vendor/autoload.php";
 define("ART_ENV", "development");
 define("ART_VIEW", __DIR__ . "/view");
 
+
+// FOR livereload
+if(ART_ENV === "development" )
+  add_filter("redirect_canonical", function(){return false;});
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'rsd_link');
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );    
+remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );    
+remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+
 Artovenry\Haml::run();
