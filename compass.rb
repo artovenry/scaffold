ec5b650d978dc067df398164f1bdfce9c174090a
artovenry <artovenry@gmail.com>
2015-10-06 16:22:35 +0900

    参照するパスを、環境非依存にした。また、　"-dev"suffixを廃止

--- compass.rb
+++ compass.rb
@@ -2,21 +2,15 @@ require 'compass/import-once/activate'
 require 'bootstrap-sass'
 require 'font-awesome-sass'
 
-sourcemap= true 
+sourcemap= false
 sass_dir= "src/scss"
 images_dir= "src/scss/img"
 css_dir= "theme/assets/css"
 fonts_dir= "theme/assets/vendor"
 generated_images_dir= "theme/assets/css/img"
 
-#FIXME
-http_images_dir= "wp-content/themes/theme/theme/css/img"
-#FIXME
-http_fonts_dir= "wp-content/themes/theme/theme/vendor"
+http_path= "./"
+http_images_dir= "img"
 
 output_style = :nested
 line_comments =  true
-
-on_stylesheet_saved do |filename|
-    File.rename(filename, "#{File.dirname(filename)}/#{File.basename(filename, ".*")}-dev.css")
-end
