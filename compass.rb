e3d37a361cfec7ca0eb4211b8ff50fdfc665be19
artovenry <artovenry@gmail.com>
2015-12-26 21:01:54 +0900

    Update compass.rb

--- compass.rb
+++ compass.rb
@@ -8,9 +8,9 @@ images_dir= "src/scss/img"
 css_dir= "theme/assets/css"
 fonts_dir= "theme/assets/vendor"
 generated_images_dir= "theme/assets/css/img"
-
+ASSET_HOST= "example.com:30000"
 #DEVELOPMENT
-asset_host= proc { |source|  "http://showtarow.local:30000"}
+asset_host= proc { |source|  ASSET_HOST}
 http_images_dir= "wp-content/themes/theme/assets/css/img"
 http_fonts_dir= "wp-content/themes/theme/assets/vendor"
 
